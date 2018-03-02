9c1e8921fde3f20a448b655615102e5b91a06f1d
Mathew Savage <mathew@mathewsavage.com>
2018-03-02 07:59:03 +0000

    Initial commit of CF Games downloader
    Need to add error handling for one bad name, which results in the JSON parser failing due to use of " in the name.
    
    Processing of raw csvs to come later

---
+++ CrossfitGames.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 07:51:54 2018
+
+@author: Mathew
+"""
+
+import requests
+import urllib
+
+import pandas as pd
+
+URL = r'https://games.crossfit.com/competitions/api/v1/competitions/open/2018/leaderboards?{params}'
+entrants = []
+scores = []
+
+PARAMS = {'division': 1,
+          'region': 0,
+          'scaled': 0,
+          'sort': 0,
+          'occupation': 0,
+          'page': 4410}
+
+def process_json(json):
+    iter_scores = []
+    iter_entrants = []
+    for row in json['leaderboardRows']:
+        iter_entrants.append(row['entrant'])
+        for score in row['scores']:
+            score['competiorId'] = row['entrant']['competitorId']
+            iter_scores.append(score)
+    return iter_entrants, iter_scores
+
+
+pages = True
+while pages:
+    data = requests.get(URL.format(params=urllib.parse.urlencode(PARAMS)))
+    if data.status_code == 200:
+        json = data.json()
+        
+        ent, sco = process_json(json)
+        entrants.extend(ent)
+        scores.extend(sco)
+        
+        pagination = json['pagination']
+        if pagination['currentPage'] == pagination['totalPages']:
+            pages = False
+        else:
+            PARAMS['page'] += 1
+
+entrants = pd.DataFrame(entrants)
+scores = pd.DataFrame(scores)
+
